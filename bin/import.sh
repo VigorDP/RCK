8d831a5704f0f1437a80007a7e9b19db10ba92a3
fengyinchao <1542963276@qq.com>
2019-03-14 00:02:27 +0800

    feat:添加impor.sh

---
+++ bin/import.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 自动生成 src/index.js 文件内容
+
+set -e
+
+TargetFile='./src/index.js'
+
+if [[ -e $TargetFile ]];then
+  rm -rf ./src/index.js
+fi
+
+echo -e "import './styles/normalize.scss';\n" >> $TargetFile
+
+file=$(find ./src -name '*.js' ! -name '*.story.js');
+
+# echo $file
+for loop in $file
+do
+  if [[ $(dirname $loop) != './src' ]];then
+    tem=${loop%\/*}
+    dir=${tem##*\/}
+    right=" from './$dir';"
+    export_state=$(grep -E 'export *' $loop)
+    left_tmp=${export_state%;*}
+    left=${left_tmp%from*}
+    final=$left$right
+    echo $final >> $TargetFile
+  fi
+done
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './styles/normalize.scss';
 
-export { Button, ButtonGroup } from './Button';
+export { Button, ButtonGroup, ButtonToolbar } from './Button';
+export { Dialog } from './Dialog';
+export { Menu, SubMenu, MenuItem, Divider } from './Menu';
+export { Tab, Tabs, TabList, TabPanel } from './Tab';
 export { Toast } from './Toast';
